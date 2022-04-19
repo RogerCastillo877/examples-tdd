4c57b7761553036a1e7fe373e7d3e62b69f0a638
RogerCastillo877 <roger.castillo877@gmail.com>
2022-04-19 15:05:01 -0500

    testing a event

---
+++ src/components/counter/counter.test.js
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Counter from "./counter";
+
+test('should display zero initial counts', () => {
+    render(<Counter />);
+
+    const result = screen.getByText(/clicked times: 0/i); 
+    expect( result ).toBeInTheDocument();
+});
+
+test('should display new counter after one click', () => {
+    render(<Counter />);
+
+    const button = screen.getByText(/click/i, { selector: 'button' });
+    fireEvent.click(button)
+
+    const result = screen.getByText(/clicked times: 1/i); 
+    expect( result ).toBeInTheDocument();
+});
